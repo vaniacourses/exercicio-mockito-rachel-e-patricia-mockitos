8d4cb269cf96b36ae12ef3499050320a0bcee3e9
Rottschaefer <rottschaefer54@gmail.com>
2025-09-15 19:51:49 -0300

    cria testes

--- src/main/java/jogo/Jogo.java
+++ src/main/java/jogo/Jogo.java
@@ -4,14 +4,15 @@ public class Jogo {
 
 	
 	/*
-	 * Se for o primeiro turno e a soma das faces dos dados cair 7 ou 11 você ganha o jogo;
-	 * Se for o primeiro turno e a soma das faces dos dados cair 2, 3 e 12 você perde o jogo; 
-	 * Se não cair nenhum desses valores, o valor é armazenado e passa para o segundo turno;
-	 * Se for o segundo turno e a soma das faces dos dados cair 7 novamente, você perde o jogo;
-	 * Se for o segundo turno, você continuando jogando os dados e só ganha se cair um numero igual ao anterior do primeiro turno;
+	 * Se for o primeiro turno e a soma das faces dos dados cair 7 ou 11 vocï¿½ ganha o jogo;
+	 * Se for o primeiro turno e a soma das faces dos dados cair 2, 3 e 12 vocï¿½ perde o jogo; 
+	 * Se nï¿½o cair nenhum desses valores, o valor ï¿½ armazenado e passa para o segundo turno;
+	 * Se for o segundo turno e a soma das faces dos dados cair 7 novamente, vocï¿½ perde o jogo;
+	 * Se for o segundo turno, vocï¿½ continuando jogando os dados e sï¿½ ganha se cair um numero igual ao anterior do primeiro turno;
 	 */
+	
 	public boolean jogo(){
-		
+
 		Jogador jogador = new Jogador();
 		Dado dadinho1 = new Dado();
 		Dado dadinho2 = new Dado();
--- src/test/java/jogo/JogoTest.java
+++ src/test/java/jogo/JogoTest.java
@@ -1,6 +1,33 @@
 package jogo;
 
+// import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 public class JogoTest {
 	
+    @Test
+    public void testarJogo() {
+
+        Jogador jogador = mock(Jogador.class);
+        Dado dado1 = mock(Dado.class);
+        Dado dado2 = mock(Dado.class);        
+
+        when(dado1.numero()).thenReturn(20);
+        when(dado2.numero()).thenReturn(14);
+
+        when(jogador.lancar(dado1, dado2)).thenReturn(34); 
+        
+        Integer a = jogador.lancar(dado1, dado2);
+        
+        // Verificar se o metodo foi chamado
+        verify(jogador).lancar(dado1, dado2);
+
+        assertEquals(a, 34);
+    }
 
 }
